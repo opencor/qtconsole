3e2f71fbd21e959ec6e2342040cd0ffd943ebe8e
David Brooks <dave@bcs.co.nz>
2017-06-06 20:56:49 +1200

    Provide a wrapper for PythonQt.

--- qtconsole/qt.py
+++ qtconsole/qt.py
@@ -1,7 +1,7 @@
 """ A Qt API selector that can be used to switch between PyQt4/5 and PySide.
 
 This uses the ETS 4.0 selection pattern of:
-PySide first, PyQt4 (API v2.) second, then PyQt5.
+PySide first, PyQt4 (API v2.) second, PyQt5 next, then PythonQt
 
 Do not use this if you need PyQt4 with the old QString/QVariant API.
 """
@@ -9,14 +9,15 @@ Do not use this if you need PyQt4 with the old QString/QVariant API.
 import os
 
 from qtconsole.qt_loaders import (load_qt, QT_API_PYSIDE, QT_API_PYSIDE2,
-                                         QT_API_PYQT, QT_API_PYQT5)
+                                  QT_API_PYQT, QT_API_PYQT5,
+                                  QT_API_PYTHONQT)
 
 QT_API = os.environ.get('QT_API', None)
-if QT_API not in [QT_API_PYSIDE, QT_API_PYSIDE2, QT_API_PYQT, QT_API_PYQT5, None]:
+if QT_API not in [QT_API_PYSIDE, QT_API_PYSIDE2, QT_API_PYQT, QT_API_PYQT5, QT_API_PYTHONQT, None]:
     raise RuntimeError("Invalid Qt API %r, valid values are: %r, %r, %r, %r" %
-                       (QT_API, QT_API_PYSIDE, QT_API_PYSIDE2, QT_API_PYQT, QT_API_PYQT5))
+                       (QT_API, QT_API_PYSIDE, QT_API_PYSIDE2, QT_API_PYQT, QT_API_PYQT5, QT_API_PYTHONQT))
 if QT_API is None:
-    api_opts = [QT_API_PYQT5, QT_API_PYSIDE2, QT_API_PYSIDE, QT_API_PYQT]
+    api_opts = [QT_API_PYTHONQT, QT_API_PYQT5, QT_API_PYSIDE2, QT_API_PYSIDE, QT_API_PYQT]
 else:
     api_opts = [QT_API]
 
